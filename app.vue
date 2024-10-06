d9d49279002fa220b821db2a5ae07760fdc4104f
Steven Eubank <eubank.steven88@gmail.com>
2024-10-06 20:14:59 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout />
 </template>
+
+<script setup>
+// No additional script needed unless you have specific logic for the app
+</script>
+
+<style scoped>
+/* Add any global styles for your app here */
+</style>
